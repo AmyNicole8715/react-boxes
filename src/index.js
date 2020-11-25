c3700c027dbbbb50198f45628e73608b84c49c0c
Amy Check <amycheck8715@gmail.com>
2020-11-25 08:15:36 -0500

    changed opacity and added background

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Box extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+    return (<div 
+        className="box"
+        style={{
+            backgroundColor: this.state.backgroundColor,
+            borderWidth: this.state.borderWidth,
+            borderColor: this.state.borderColor,
+            borderStyle: this.state.borderStyle,
+            opacity: this.state.opacity,
+            width: this.state.width,
+            height: this.state.height,
+        }}
+        onMouseEnter={() => this.handleMouseEnter()}
+        onMouseLeave={() => this.handleMouseLeave()}
+        ></div>);
+    }
+
+    handleMouseEnter() {
+        const r = Math.floor(Math.random() * 255);
+        const g = Math.floor(Math.random() * 255);
+        const b = Math.floor(Math.random() * 255);
+        const colorString = `rgb(${r}, ${g}, ${b})`;
+        this.setState({backgroundColor: colorString});
+        this.setState({borderWidth: '3px'});
+        this.setState({width: '94px'});
+        this.setState({height: '94px'});
+        this.setState({opacity: '0.5'});
+    }
+
+    handleMouseLeave() {
+        this.setState({borderWidth: '0px'});
+        this.setState({width: '100px'});
+        this.setState({height: '100px'});
+    }
+}
+
+class Grid extends React.Component {
+    render() {
+        const boxes = Array(100).fill(null);
+        return (
+            <div className="grid">
+                {boxes.map((box) => <Box />)}
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Grid />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
